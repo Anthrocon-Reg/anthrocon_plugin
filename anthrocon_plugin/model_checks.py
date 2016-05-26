e22c06f31cf9bc8bc70e386e83f232774b03b122
Victoria Earl <kitsuta@gmail.com>
2016-05-26 06:03:19 +0200

    Fix 71 (#77)
    * Hide or disable luncheon fields at-con
    Fixes https://github.com/Anthrocon-Reg/anthrocon_plugin/issues/71. We only disable fields for people editing their badges so they can see what they had selected.

--- anthrocon_plugin/model_checks.py
+++ anthrocon_plugin/model_checks.py
@@ -29,6 +29,16 @@ def cellphone_required(attendee):
     if not attendee.cellphone:
         return "Please enter your phone number."
 
+@prereg_validation.Attendee
+def no_luncheon_at_con(attendee):
+    if c.AT_THE_CON and attendee.luncheon_going and not attendee.orig_value_of('luncheon_going'):
+        return "Sorry, it is too late to get into the Supersponsor luncheon!"
+
+@prereg_validation.Attendee
+def no_more_luncheon_tickets_at_con(attendee):
+    if c.AT_THE_CON and attendee.luncheon_tickets > attendee.orig_value_of('luncheon_tickets'):
+        return "Sorry, you cannot buy any more luncheon tickets!"
+
 @prereg_validation.Group
 def dealer_website(group):
     pass
